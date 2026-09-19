924418dfdeb402ec230777b3fc9c1701e7773409
Riya <riyasingh9637@gmail.com>
2026-09-19 20:33:26 +0530

    complete public website

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+
+            if user.role == 'ADMIN':
+                return redirect('/admin/')
+
+            elif user.role == 'FACULTY':
+                return redirect('faculty_dashboard')
+
+            elif user.role == 'STUDENT':
+                return redirect('student_dashboard')
+
+        return render(request, 'accounts/login.html', {
+            'error': 'Invalid username or password.'
+        })
+
+    return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from core.views import home, courses, contact, class_detail, about, announcements
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', home, name='home'),
-    path('courses/', courses, name='courses'),
-    path('contact/', contact, name='contact'),
-    path('about/', about, name='about'),
-    path('announcements/', announcements, name='announcements'),
-    path('class/<int:class_no>/', class_detail, name='class_detail'),
+    path('', include('core.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
